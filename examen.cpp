7b53841887f5c4309ff34d510a830e3412d96b49
walterfl1 <00111925@uca.edu.sv~>
2025-11-30 22:56:50 -0600

    meeeeeee

--- examen.cpp
+++ examen.cpp
@@ -73,3 +73,16 @@ int main() {
         std::cout << id << " ";
     }
     std::cout << std::endl;
+
+
+
+
+    # 1. Primer commit
+git add .
+git commit -m "primer commit"
+
+# 2. Crear una nueva rama
+git checkout -b nombre-de-la-rama
+
+# 3. Subir la nueva rama a GitHub
+git push -u origin nombre-de-la-rama
